0a4264b603a907875b17030a5c45f113b3467022
Chinmay Pareek <cpareek@opentext.com>
2023-11-10 19:47:30 +0530

    episode3-part1

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag"),
-  ])
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React Element = React.createElement => Object => HTMLElement(render), Creating a React Element using core react
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React 🚀"
 );
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyx: "abc" },
-//   "Hello World from React"
-// );
+console.log(heading);
+
+//JSX - not html in js, It is html like syntax - Creating a react element using JSX
+//This JSX will be transpiled before it reaches to the JS engine - PARCEL - BABEL - It takes your JSX and converts it onto the code which browsers can understand
+const jsxHeading = <h1 id="heading">Namaste React using JSX 🚀</h1>;
+
+console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 Namaste React.
+
+#Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification of code | Bundling | Bundles the code | compress the file
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Https
+- Tree Shaking - Remove unused code
